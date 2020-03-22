c564618d98589621f1e29f4a11a3814c5826787b
avi hadad <avih75@gmail.com>
2020-03-23 01:45:28 +0200

    promises problem

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,190 @@
 define(["require", "exports", "TFS/TestManagement/RestClient", "VSS/Controls", "VSS/Controls/Grids"], function (require, exports, TestRestClient, Controls, Grids) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    var TestGrid = (function () {
-        function TestGrid() {
+    var TestPointModel = (function () {
+        function TestPointModel() {
         }
-        return TestGrid;
+        return TestPointModel;
     }());
-    function getProjectTestPlanes() {
-        $("#selectPlan").append(new Option("fake", "1"));
+    var TestCaseModel = (function () {
+        function TestCaseModel() {
+        }
+        return TestCaseModel;
+    }());
+    var TestSuiteModel = (function () {
+        function TestSuiteModel() {
+        }
+        return TestSuiteModel;
+    }());
+    var client;
+    function Init_Page() {
+        client = TestRestClient.getClient();
+        var selectPlan = $("#selectPlan");
+        $("#graph-container").hide();
+        $("#grid-container").hide();
+        $("#PlanInfos").hide();
+        selectPlan.hide();
         var projectName = VSS.getWebContext().project.name;
-        var client = TestRestClient.getClient();
+        BuildRadioButton();
+        BuildSelect(projectName, selectPlan);
+    }
+    function BuildRadioButton() {
+        $('input[type=radio][name=view]').change(function () {
+            if (this.value == 'Table') {
+                $("#PlanInfos").show();
+                $("#grid-container").show();
+                $("#graph-container").hide();
+            }
+            else if (this.value == 'Graphs') {
+                $("#grid-container").hide();
+                $("#PlanInfos").hide();
+                $("#graph-container").show();
+            }
+        });
+    }
+    function BuildSelect(projectName, selectPlan) {
+        selectPlan.change(function () {
+            var selectedPlane = $(this).children("option:selected").val();
+            BuildTableTestGrid(projectName, selectedPlane);
+            BuildGraph(selectedPlane);
+        });
         client._setInitializationPromise(client.authTokenManager.getAuthToken());
         client.getPlans(projectName).then(function (plans) {
+            var lastPlan;
             plans.forEach(function (plan) {
-                $("#selectPlan").append(new Option(plan.name, plan.id.toString()));
+                selectPlan.append(new Option(plan.name, plan.id.toString()));
+                lastPlan = plan.id;
             });
+            $("#loading").hide();
+            selectPlan.show();
+            BuildTableTestGrid(projectName, lastPlan);
+            BuildGraph(lastPlan);
         });
-        $("#loading").hide();
     }
-    exports.getProjectTestPlanes = getProjectTestPlanes;
-    function getSuiteTestPoint(source, testPlaneId, suiteId, target) {
+    function BuildTableTestGrid(projectName, testPlaneId) {
         var container = $("#grid-container");
-        var gridOptions = {
-            height: "600px",
-            width: "10000",
-            source: source,
-            columns: [
-                { text: "Test run Id", width: 200, index: "id" },
-                { text: "Test Name", width: 500, index: "name" },
-                { text: "State", width: 300, index: "state" }
-            ]
-        };
-        var source = new Array();
-        var grid = Controls.create(Grids.Grid, container, gridOptions);
+        var planInfo = $("#PlanInfos");
+        container.empty();
+        planInfo.empty();
+        client.getPlanById(projectName, +testPlaneId)
+            .then(function (selectedPlane) { return GetTestPlaneInfo(selectedPlane, testPlaneId, planInfo, projectName); })
+            .then(function (palneFullInfo) { return ReArangeSuiteList(palneFullInfo); })
+            .then(function (palneFullInfo) { return CreateTableView(palneFullInfo, container); });
     }
-    exports.getSuiteTestPoint = getSuiteTestPoint;
+    function GetTestPlaneInfo(selectedPlane, testPlaneId, planInfo, projectName) {
+        planInfo.append($("<h4 />").text("project: " + projectName +
+            "    Plane: " + testPlaneId +
+            "    Root Suite: " + selectedPlane.rootSuite.name +
+            "    Iteration: " + selectedPlane.iteration +
+            "    Start Date: " + selectedPlane.startDate +
+            "    State: " + selectedPlane.state));
+        return GetTestSuiteInfos(projectName, testPlaneId)
+            .then(function (palneFullInfo) { return palneFullInfo; });
+    }
+    function GetTestSuiteInfos(projectName, testPlaneId) {
+        var palneFullInfo = new Array();
+        return client.getTestSuitesForPlan(projectName, +testPlaneId).then(function (suites) {
+            suites.forEach(function (suite) {
+                var newSuite = new TestSuiteModel();
+                newSuite.suiteId = suite.id;
+                try {
+                    newSuite.perentId = suite.parent.id;
+                }
+                catch (_a) {
+                    newSuite.perentId = "0";
+                }
+                ;
+                newSuite.suiteName = suite.name;
+                newSuite.suiteState = suite.state;
+                newSuite.childrenSuites = Array();
+                TestCaseInfos(projectName, testPlaneId, suite.id.toString())
+                    .then(function (testCases) {
+                    newSuite.testCaseList = testCases;
+                })
+                    .then(function () {
+                    return PointsInfos(projectName, testPlaneId, newSuite.suiteId.toString());
+                }).then(function (testPointList) {
+                    newSuite.testpoints = testPointList;
+                })
+                    .then(function () {
+                    palneFullInfo.push(newSuite);
+                });
+            });
+        }).then(function () {
+            return palneFullInfo;
+        });
+    }
+    function TestCaseInfos(projectName, testPlaneId, suiteId) {
+        var TestCaseList = new Array();
+        return client.getTestCases(projectName, +testPlaneId, +suiteId).then(function (testCases) {
+            testCases.forEach(function (testCase) {
+                var pointTesterName;
+                var pointConfigurationName;
+                testCase.pointAssignments.forEach(function (point) {
+                    pointTesterName = point.tester.uniqueName;
+                    pointConfigurationName = point.configuration.name;
+                });
+                var newTestCase = {
+                    lastTestPoint: new TestPointModel,
+                    testCaseName: testCase.testCase.name,
+                    pointTesterName: pointTesterName,
+                    pointConfigurationName: pointConfigurationName
+                };
+                TestCaseList.push(newTestCase);
+            });
+        }).then(function () {
+            return TestCaseList;
+        });
+    }
+    function PointsInfos(projectName, testPlaneId, suiteId) {
+        var testPointList = new Array();
+        return client.getPoints(projectName, +testPlaneId, +suiteId).then(function (points) {
+            points.forEach(function (point) {
+                testPointList.push({
+                    suite: point.suite.name,
+                    testCase: point.testCase.name,
+                    state: point.lastResultState,
+                    outCome: point.outcome,
+                    lastTestRun: point.lastTestRun.name,
+                    assignedTo: point.assignedTo.displayName,
+                    comment: point.comment,
+                    failureType: point.failureType,
+                });
+            });
+        }).then(function () {
+            return testPointList;
+        });
+    }
+    function ReArangeSuiteList(palneFullInfo) {
+        return palneFullInfo;
+    }
+    function CreateTableView(palneFullInfo, container) {
+        {
+            var gridOptions = {
+                height: "600px",
+                width: "17000",
+                source: palneFullInfo,
+                columns: [
+                    { text: "Suite", width: 200, index: "suite" },
+                    { text: "Test Case", width: 200, index: "testCase" },
+                    { text: "State", width: 100, index: "state" },
+                    { text: "Out-Come", width: 100, index: "outCome" },
+                    { text: "Last Test Run", width: 200, index: "lastTestRun" },
+                    { text: "Assigned-To", width: 200, index: "assignedTo" },
+                    { text: "Comment", width: 500, index: "comment" },
+                    { text: "Failure Type", width: 200, index: "failureType" }
+                ]
+            };
+            var target = Controls.create(Grids.Grid, container, gridOptions);
+            target.setDataSource(palneFullInfo);
+        }
+    }
+    function BuildGraph(testPlaneId) {
+        var container = $("#graph-container");
+        container.empty();
+    }
     var id = VSS.getContribution().id;
-    VSS.register(id, getProjectTestPlanes);
-    getProjectTestPlanes();
+    VSS.register(id, Init_Page);
+    VSS.resize();
+    Init_Page();
 });
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,54 +1,200 @@
-import TestRestClient = require("TFS/TestManagement/RestClient"); 
+import TestRestClient = require("TFS/TestManagement/RestClient");
 import Controls = require("VSS/Controls");
 import Grids = require("VSS/Controls/Grids");
-import { TestPlan } from "TFS/TestManagement/Contracts";
-import { VssConnection, VssService } from "VSS/Service";
-
-class TestGrid {
-    id: number;
-    name: string;
-    outcome: string;
+import { TestPlan, TestSuite } from "TFS/TestManagement/Contracts";
+class TestPointModel {
+    state: string;
+    lastTestRun: string;
+    suite: string;
+    outCome: string;
+    assignedTo: string;
+    comment: string;
+    failureType: string;
+    testCase: string;
 }
-
-export function getProjectTestPlanes(): void {  
-    $("#selectPlan").append(new Option("fake","1")); 
-    // add on selected
+class TestCaseModel {
+    lastTestPoint: TestPointModel;
+    testCaseName: string;
+    pointTesterName: string;
+    pointConfigurationName: string;
+}
+class TestSuiteModel {
+    suiteId: number;
+    perentId: string;
+    suiteName: string;
+    suiteState: string;
+    childrenSuites: Array<TestSuiteModel>;
+    testCaseList: Array<TestCaseModel>;
+    testpoints: Array<TestPointModel>;
+}
+let client: TestRestClient.TestHttpClient4_1;
+function Init_Page(): void {
+    client = TestRestClient.getClient();
+    let selectPlan = $("#selectPlan");
+    $("#graph-container").hide();
+    $("#grid-container").hide();
+    $("#PlanInfos").hide();
+    selectPlan.hide();
     var projectName = VSS.getWebContext().project.name;
-    let client = TestRestClient.getClient();
-    client._setInitializationPromise(client.authTokenManager.getAuthToken());    
-    client.getPlans(projectName).then((plans) => {
-        plans.forEach(plan => { 
-            $("#selectPlan").append(new Option(plan.name,plan.id.toString())); 
-        });
-    })  
-    $("#loading").hide();
-    // set selected plane
-    // getSuiteTestPoint(source, 31, 31, grid);
+    BuildRadioButton()
+    BuildSelect(projectName, selectPlan);
 }
-
-export function getSuiteTestPoint(source: Array<TestGrid>, testPlaneId: number, suiteId: number, target: Grids.Grid): void {
-    var container = $("#grid-container"); 
-    var gridOptions: Grids.IGridOptions = {
-        height: "600px",
-        width: "10000",
-        source: source,
-        columns: [
-            { text: "Test run Id", width: 200, index: "id" },
-            { text: "Test Name", width: 500, index: "name" },
-            { text: "State", width: 300, index: "state" }
-        ]
-    };
-    var source = new Array<TestGrid>();
-    var grid = Controls.create(Grids.Grid, container, gridOptions);
-    // client.getPoints(getTeamContext().projectname, testPlaneId, suiteId).then(TestPoints => {
-    //     TestPoints.forEach(testPoint => {
-    //         source.push({ id: testPoint.id, name: testPoint.testCase.name, outcome: testPoint.outcome });
-    //     })
-    //     target.setDataSource(source);
-    // }
-    // );
+function BuildRadioButton() {
+    $('input[type=radio][name=view]').change(function () {
+        if (this.value == 'Table') {
+            $("#PlanInfos").show();
+            $("#grid-container").show();
+            $("#graph-container").hide();
+        }
+        else if (this.value == 'Graphs') {
+            $("#grid-container").hide();
+            $("#PlanInfos").hide();
+            $("#graph-container").show();
+        }
+    });
+}
+function BuildSelect(projectName: string, selectPlan: JQuery) {
+    selectPlan.change(function () {
+        let selectedPlane = $(this).children("option:selected").val();
+        BuildTableTestGrid(projectName, selectedPlane);
+        BuildGraph(selectedPlane);
+    });
+    client._setInitializationPromise(client.authTokenManager.getAuthToken());
+    client.getPlans(projectName).then((plans) => {
+        let lastPlan: any;
+        plans.forEach(plan => {
+            selectPlan.append(new Option(plan.name, plan.id.toString()));
+            lastPlan = plan.id;
+        });
+        $("#loading").hide();
+        selectPlan.show();
+        BuildTableTestGrid(projectName, lastPlan);
+        BuildGraph(lastPlan);
+    })
+}
+function BuildTableTestGrid(projectName: string, testPlaneId: string): void {
+    let container = $("#grid-container");
+    let planInfo = $("#PlanInfos");
+    container.empty();
+    planInfo.empty();
+    client.getPlanById(projectName, +testPlaneId)
+        .then((selectedPlane) => GetTestPlaneInfo(selectedPlane, testPlaneId, planInfo, projectName))
+        .then((palneFullInfo) => ReArangeSuiteList(palneFullInfo))
+        .then((palneFullInfo) => CreateTableView(palneFullInfo, container));
+}
+function GetTestPlaneInfo(selectedPlane: TestPlan, testPlaneId: string, planInfo: JQuery, projectName: string) {
+    planInfo.append($("<h4 />").text("project: " + projectName +
+        "    Plane: " + testPlaneId +
+        "    Root Suite: " + selectedPlane.rootSuite.name +
+        "    Iteration: " + selectedPlane.iteration +
+        "    Start Date: " + selectedPlane.startDate +
+        "    State: " + selectedPlane.state));
+    return GetTestSuiteInfos(projectName, testPlaneId)
+        .then((palneFullInfo) => { return palneFullInfo; });
+}
+function GetTestSuiteInfos(projectName: string, testPlaneId: string) {
+    let palneFullInfo: Array<TestSuiteModel> = new Array<TestSuiteModel>();
+    return client.getTestSuitesForPlan(projectName, +testPlaneId).then((suites) => {
+        suites.forEach(suite => {
+            let newSuite: TestSuiteModel = new TestSuiteModel();
+            newSuite.suiteId = suite.id;
+            try {
+                newSuite.perentId = suite.parent.id;
+            }
+            catch {
+                newSuite.perentId = "0";
+            };
+            newSuite.suiteName = suite.name;
+            newSuite.suiteState = suite.state;
+            newSuite.childrenSuites = Array<TestSuiteModel>();
+            TestCaseInfos(projectName, testPlaneId, suite.id.toString())
+                .then((testCases) => {
+                    newSuite.testCaseList = testCases;
+                })
+                .then(() => {
+                    return PointsInfos(projectName, testPlaneId, newSuite.suiteId.toString());
+                }).then((testPointList) => {
+                    newSuite.testpoints = testPointList;
+                })
+                .then(() => {
+                    palneFullInfo.push(newSuite);
+                })
+        })
+    }).then(() => {
+        return palneFullInfo;
+    })
+}
+function TestCaseInfos(projectName: string, testPlaneId: string, suiteId: string) {
+    let TestCaseList = new Array<TestCaseModel>();
+    return client.getTestCases(projectName, +testPlaneId, +suiteId).then((testCases) => {
+        testCases.forEach(testCase => {
+            let pointTesterName: string;
+            let pointConfigurationName: string;
+            testCase.pointAssignments.forEach(point => {
+                pointTesterName = point.tester.uniqueName;
+                pointConfigurationName = point.configuration.name;
+            });
+            let newTestCase: TestCaseModel = {
+                lastTestPoint: new TestPointModel,
+                testCaseName: testCase.testCase.name,
+                pointTesterName: pointTesterName,
+                pointConfigurationName: pointConfigurationName
+            }
+            TestCaseList.push(newTestCase);
+        });
+    }).then(() => {
+        return TestCaseList
+    })
+}
+function PointsInfos(projectName: string, testPlaneId: string, suiteId: string) {
+    let testPointList = new Array<TestPointModel>();
+    return client.getPoints(projectName, +testPlaneId, +suiteId).then((points) => {
+        points.forEach(point => {
+            testPointList.push({
+                suite: point.suite.name,
+                testCase: point.testCase.name,
+                state: point.lastResultState,
+                outCome: point.outcome,
+                lastTestRun: point.lastTestRun.name,
+                assignedTo: point.assignedTo.displayName,
+                comment: point.comment,
+                failureType: point.failureType,
+            });
+        });
+    }).then(() => {
+        return testPointList;
+    })
+}
+function ReArangeSuiteList(palneFullInfo: Array<TestSuiteModel>) {
+    return palneFullInfo;
+}
+function CreateTableView(palneFullInfo: Array<TestSuiteModel>, container: JQuery) {
+    {
+        // try to show the info
+        var gridOptions: Grids.IGridOptions = {
+            height: "600px",
+            width: "17000",
+            source: palneFullInfo,
+            columns: [
+                { text: "Suite", width: 200, index: "suite" },
+                { text: "Test Case", width: 200, index: "testCase" },
+                { text: "State", width: 100, index: "state" },
+                { text: "Out-Come", width: 100, index: "outCome" },
+                { text: "Last Test Run", width: 200, index: "lastTestRun" },
+                { text: "Assigned-To", width: 200, index: "assignedTo" },
+                { text: "Comment", width: 500, index: "comment" },
+                { text: "Failure Type", width: 200, index: "failureType" }
+            ]
+        };
+        var target = Controls.create(Grids.Grid, container, gridOptions);
+        target.setDataSource(palneFullInfo);
+    }
+}
+function BuildGraph(testPlaneId: string) {
+    var container = $("#graph-container");
+    container.empty();
 }
 var id = VSS.getContribution().id;
-VSS.register(id, getProjectTestPlanes);
-
-getProjectTestPlanes();
+VSS.register(id, Init_Page);
+VSS.resize();
+Init_Page();
