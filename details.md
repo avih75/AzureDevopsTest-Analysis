0ae7119ff5cb2ee60ba0a854443c8972fd8f7472
avi hadad <avih75@gmail.com>
2020-04-14 16:37:42 +0300

    fix

--- details.md
+++ details.md
@@ -1,1 +1,15 @@
-How to get started
+# Make Azure Test Plan more Frendly
+
+![Layout Customization](images/A (1).png)
+
+# Get Test Plan Detaily Info
+
+![Layout Customization](images/A (2).png)
+
+# Watch Test Suite Details
+
+![Layout Customization](images/A (3).png)
+
+# Control Your Tests
+
+![Layout Customization](images/A (4).png)
--- scripts/SecondApp.ts
+++
@@ -1,7 +0,0 @@
-// import TestMTestPlan = require("TFS/TestManagement/VSS.TestPlan.WebApi");
-// import TestManagment = require("TFS/TestManagement/VSS.Tcm.WebApi");
-// import TestManagmentt = require("TFS/WorkItemTracking/RestClient"); 
-
-// let y = TestManagmentt.getClient(); 
-// y.getWorkItem()
-// let x = TestMTestPlan.getClient(); 
